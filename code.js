289d90bde50f1d81e9525576d4407bcdd39bf84c
zikes28 <you@example.com>
2021-04-27 16:26:44 -0500

    :video_camera: first commit testing upload again + still working on this extention. trying to build chrome extention that can download clips from youtube videos

---
+++ code.js
@@ -0,0 +1,4 @@
+document.getElementById("button")
+            .addEventListener("click", function() {
+                $(".download-video").html('<iframe style="width:230px;height:60px;border:0;overflow:hidden;" scrolling="no" src="https://loader.to/api/button/?url='+"https://www.youtube.com/watch?v=PPFVK3sV8Zk"+'&f='+"360"+'"></iframe>')
+            }, false);
